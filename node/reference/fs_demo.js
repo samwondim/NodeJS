ae4da7442d88ba537af1e6a475c01953eca5b5eb
unknown <samwondim@gmail.com>
2023-06-24 08:02:43 +0300

    added filesystem and operating system demo files.

--- node/reference/fs_demo.js
+++ node/reference/fs_demo.js
@@ -2,7 +2,32 @@ const path = require('path');
 const fs = require('fs');
 
 // create folder
-fs.mkdir(path.join(__dirname, '/test'), {}, err => {
-    if (err) throw err;
-    console.log('Folder created');
-});
+// fs.mkdir(path.join(__dirname, 'test'), {}, (err) => {
+//     if (err) throw err;
+//     console.log('Folder created!');
+// });
+
+// create and write to file
+// fs.writeFile(path.join(__dirname, '/test', 'hello.txt'), 'Hello World!', err => {
+//     if (err) throw err;
+//     console.log('file created');
+
+//     // append to file
+//     fs.appendFile(path.join(__dirname, '/test', 'hello.txt'), ' I love nodeJs.', (err) => {
+//         if (err) throw err;
+//         console.log('appended to file');
+//     });
+// });
+
+// read from file
+// fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// });
+
+// rename filename
+fs.rename(path.join(__dirname, '/test', 'hello.txt'),
+    path.join(__dirname, '/test', 'helloworld.txt'), err => {
+        if (err) throw err;
+        console.log('file renamed');
+    });
